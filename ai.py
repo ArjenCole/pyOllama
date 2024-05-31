ebbcddecf835e79c16fc614557e2e952081ef1ec
smedi1660 <jinhesky@hotmail.com>
2024-05-31 10:24:44 +0800

    抽象出sendtoAI函数，减少重复代码

--- ai.py
+++ ai.py
@@ -6,49 +6,26 @@ app = Flask(__name__)
 
 @app.route('/ai', methods=['POST'])
 def chat_ai():
-    print("11111")
-
     # 获取前端发送的 JSON 数据
     user_input = request.json.get('prompt', '')
 
-    # 构建请求体
-    tData = {
-        "model": "qwen:7b",
-        "prompt": user_input,
-        "stream": False
-    }
-    print("2")
-
-    # 发送请求到 AI 模型服务
-    try:
-        tResponse = requests.post("http://127.0.0.1:11434/api/generate", json=tData)
-        tResponse.raise_for_status()  # 如果响应状态码不是 200，将抛出异常
-    except requests.exceptions.RequestException as e:
-        return jsonify({'error': 'An error occurred while communicating with the AI model service.'}), 500
-    print("3")
-
-    # 检查请求是否成功
-    if tResponse.status_code == 200:
-        print("4")
-
-        tDict = tResponse.json()
-        print(tDict['response'])
-        return jsonify({'response': tDict['response']})
-    else:
-        return jsonify({'error': f"Request failed with status code: {tResponse.status_code}"}), tResponse.status_code
+    return sendtoAI(user_input)
 
 
 def excel_ai(pList):
-
     # 获取前端发送的 JSON 数据
-    user_input = "下面是一组字符串列表，请告诉我这些字符串中，字面意思最接近建筑工程的字符串出现在第几个:"
+    user_input = "你是一个字符串匹配器，可以对字符串的字面意思进行匹配；下面是一组字符串列表，请告诉我这些字符串中，《建筑工程》出现在第几个:"
     for feStr in pList:
         user_input = user_input + "\n" + str(feStr)
 
+    return sendtoAI(user_input)
+
+
+def sendtoAI(pUserInput):
     # 构建请求体
     tData = {
         "model": "qwen:7b",
-        "prompt": user_input,
+        "prompt": pUserInput,
         "stream": False
     }
 
@@ -61,13 +38,13 @@ def excel_ai(pList):
 
     # 检查请求是否成功
     if tResponse.status_code == 200:
-        print("4")
 
         tDict = tResponse.json()
         print(tDict['response'])
         return jsonify({'response': tDict['response']})
     else:
         return jsonify({'error': f"Request failed with status code: {tResponse.status_code}"}), tResponse.status_code
+
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=8080)
--- pyExcel.py
+++ pyExcel.py
@@ -15,7 +15,7 @@ def extract_headers(filename, sheetname=None):
     header_data = []
 
     # 假设表头在前几行，检测包含文字的单元格
-    for row in ws.iter_rows(min_row=1, max_row=10):  # 假设表头在前4行之内
+    for row in ws.iter_rows(min_row=1, max_row=7):  # 假设表头在前4行之内
         for cell in row:
             if cell.value is not None:
                 header_data.append(cell.value)
