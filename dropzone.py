07f66f51ce505fc2da6b41695f3f414eb4c872b1
Arjen Cole-DESKTOP-smedi-2024 <jinhesky@hotmail.com>
2024-09-05 15:48:24 +0800

    规范上传文件地址
    1.设置UPLOADS_DIR 常量存放上传文件地址
    2.保存文件功能单独放在file_save函数中
    3.后续将excel文件处理单独做一个函数

--- dropzone.py
+++ dropzone.py
@@ -10,6 +10,7 @@ from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
 CORS(app)  # 允许跨源请求
+UPLOADS_DIR = 'F:/GithubRepos/ArjenCole/pyOllama/uploads'  #上传文件存储地址
 
 
 @app.route('/dropzone')
@@ -24,8 +25,9 @@ def dropzone_upload():
     # file = request.files.get('file')
     _file = request.files['file0']
     # print('filename', _file.filename)
-    _dict = file_process(_file)
-    if 'message' in _dict.keys():
+    _dict = file_save(_file)
+    if 'DIR' in _dict.keys():
+        pyExcel.get_workbook(_dict['DIR'])
         _raw_word = '单价\r\n（元）'
         _matched_word = pyFCM.fuzzy_match(_raw_word)
         print(f"识别的结论: '{_raw_word}' 与 '{_matched_word}'匹配")
@@ -34,19 +36,18 @@ def dropzone_upload():
         return jsonify({'error': '上传失败，未知错误'})
 
 
-def file_process(p_file):
+def file_save(p_file):
     if p_file.filename == '':
         return {'error': '没有选择文件'}
     if p_file:
-        if not os.path.exists('F:/GithubRepos/ArjenCole/pyOllama/uploads'):
-            os.makedirs('F:/GithubRepos/ArjenCole/pyOllama/uploads')  # 确保目录存在
+        if not os.path.exists(UPLOADS_DIR):
+            os.makedirs(UPLOADS_DIR)  # 确保目录存在
         # filename = secure_filename(file.filename)  # 使用 Werkzeug 库提供的 secure_filename 函数
         _file_name = p_file.filename
-        _file_path = os.path.join('F:/GithubRepos/ArjenCole/pyOllama/uploads', _file_name)
+        _file_path = os.path.join(UPLOADS_DIR, _file_name)
         p_file.save(_file_path)
         print(_file_path)
-        pyExcel.get_workbook(_file_path)
-        return {'message': '文件上传成功！~'}
+        return {'DIR': _file_path}
 
 
 if __name__ == '__main__':
