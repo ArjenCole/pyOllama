dc283ed0d7e9d6bcc3f1b0b896fc6fb64a25baff
Arjen Cole-DESKTOP-smedi-2024 <jinhesky@hotmail.com>
2024-09-09 15:49:07 +0800

    增加新dataframe功能

--- pyExcel.py
+++ pyExcel.py
@@ -2,11 +2,15 @@ import openpyxl
 import pandas as pd
 
 
+def new_workbook():
+    return pd.DataFrame()
+
+
 def get_workbook(p_file_path):
     _data_frame_dict = pd.read_excel(p_file_path, sheet_name=None)
     return _data_frame_dict
 
 
-def table_format(p_dict,p_data_frame):
+def table_format(p_dict, p_data_frame):
     _json = {"row": 5, "column": 6}
     print(type(_json))
