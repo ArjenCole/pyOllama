79b65047732620118f1e192a90161141be5c6aa6
Arjen Cole-DESKTOP-smedi-2024 <jinhesky@hotmail.com>
2024-09-20 11:22:53 +0800

    匹配最匹配的表单，输出匹配的行

--- dropzone.py
+++ dropzone.py
@@ -27,14 +27,17 @@ def dropzone_upload():
     # print('filename', _file.filename)
     _dict = file_save(_file)
     if 'DIR' in _dict.keys():
-        _work_book_similarity = workbook_similarity(_dict['DIR'])
+        _match_sheet_name, _match_sheet_row = workbook_similarity(_dict['DIR'])
+        print('匹配sheet', _match_sheet_name, _match_sheet_row)
         # _work_book = pyExcel.get_workbook(_dict['DIR'])
 
+        '''
         _raw_word = '建  筑\r\n工  程'
         _matched_word, similarity_score, = pyFCM.fuzzy_match(_raw_word)
-        print(f"识别的结论: '{_raw_word}' 与 '{_matched_word}'匹配，匹配度'{similarity_score}")
+        print(f"识别的结论: '{_raw_word}' 与 '{_matched_word}'匹配，匹配度'{similarity_score}")        
         return jsonify({'message': f"识别的结论: '{_raw_word}' 与 '{_matched_word}'匹配"})
-
+        '''
+        return None
     else:
         return jsonify({'error': '上传失败，未知错误'})
 
@@ -55,29 +58,60 @@ def file_save(p_file):
 
 def workbook_similarity(p_dir):
     _work_book = pyExcel.get_workbook(p_dir)
-    rt_work_book = pyExcel.new_workbook()
+
+    _max_similarity = 0
+    rt_match_sheet_name = None
+    rt_match_sheet_row = 0
 
     for fe_sheet_name in _work_book.keys():
         _work_sheet = _work_book[fe_sheet_name]
-        rt_work_book.update({fe_sheet_name: _work_sheet})
+        _sheet_match_row, _sheet_similarity = worksheet_similarity(_work_sheet)
+        if _sheet_similarity > _max_similarity:
+            _max_similarity = _sheet_similarity
+            rt_match_sheet_name = fe_sheet_name
+            rt_match_sheet_row = _sheet_match_row
 
+        '''
         for fe_row in range(0, _work_sheet.shape[0]):
             for fe_col in range(0, _work_sheet.shape[1]):
                 # print(fe_row, fe_col, _work_sheet.iloc[fe_row][fe_col])
                 _similarity = match_f4(_work_sheet.iloc[fe_row][fe_col])
                 rt_work_book[fe_sheet_name].iloc[fe_row][fe_col] = _similarity
                 print(fe_row, fe_col, _work_sheet.iloc[fe_row][fe_col], rt_work_book[fe_sheet_name].iloc[fe_row][fe_col], _similarity)
-    return rt_work_book
+        '''
+    return rt_match_sheet_name, rt_match_sheet_row
+
+
+def worksheet_similarity(p_sheet):
+    print("表单", p_sheet)
+
+    rt_match_row = 0
+    rt_max_similarity = 0
+    for fe_row in range(0, min(20, p_sheet.shape[0])):
+        _f4_similarity_array = list(range(21))
+        for fe_col in range(0, min(20, p_sheet.shape[1])):
+            print("行列", fe_row, fe_col, p_sheet.iloc[fe_row][fe_col])
+            f4_similarity = match_f4(p_sheet.iloc[fe_row][fe_col])
+            _f4_similarity_array[fe_col] = f4_similarity
+            _row_similarity = 0
+            if fe_col >= 3:
+                _row_similarity = (_f4_similarity_array[fe_col] + _f4_similarity_array[fe_col - 1]
+                                   + _f4_similarity_array[fe_col - 2] + _f4_similarity_array[fe_col - 3])
+            if _row_similarity > rt_max_similarity:
+                rt_max_similarity = _row_similarity
+                rt_match_row = fe_row
+    return rt_match_row, rt_max_similarity
 
 
 def match_f4(p_raw_word):
+    # ('match_f4', p_raw_word)
     _target_words = ['建筑工程', '安装工程', '设备及工器具购置费', '其他费用']
 
     _matched_word, rt_similarity_score, = pyFCM.fuzzy_match(p_raw_word, _target_words)
     if rt_similarity_score is not None:
         return rt_similarity_score[0][0]
     else:
-        return None
+        return 0
 
 
 if __name__ == '__main__':
