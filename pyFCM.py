1938798232bb93da37370cf8e9176e3d1f694c91
Arjen Cole-DESKTOP-smedi-2024 <jinhesky@hotmail.com>
2025-04-25 13:26:23 +0800

    完善输入表没有材质列时管配件的识别

--- pyFCM.py
+++ pyFCM.py
@@ -60,21 +60,38 @@ def test_para(para):
 
 
 MATERIAL_FITTINGS = ["Q235A", "Q235B", "Q235C", "Q235D", "Q235E", "钢",
-                     "SS304", "SS316", "球铁", "橡胶", "PE100", "PVC-U", "nan"]
-MATERIAL_STEEL_FITTINGS = ["Q235A", "Q235B", "Q235C", "Q235D", "Q235E", "钢", "SS304", "SS316"]
+                     "SS304", "SS316", "球铁", "橡胶", "塑料", "PE100", "PVC-U", "nan"]
+MATERIAL_FITTINGS_STEEL = ["Q235A", "Q235B", "Q235C", "Q235D", "Q235E", "钢", "SS304", "SS316"]
 material_type = ["管配件", "阀门", "设备", "材料"]
+
+def _fuzzy_match_material(pEquipmentMaterial):
+    rtMaterial, rtScore = "", 0.00
+    if pEquipmentMaterial.material != "":
+        rtMaterial, rtScore = process.extractOne(pEquipmentMaterial.material, MATERIAL_FITTINGS)  # 先匹配材质
+    else:
+        tMatch = re.compile(r"de\d+").search(pEquipmentMaterial.specification)
+        if tMatch is not None:
+            rtMaterial, rtScore = "塑料", 90
+    return rtMaterial, rtScore
 
 
 def fuzzy_match_EM(pEquipmentMaterial):
     rtMaterial, tScore = "", 0.00
-    if pEquipmentMaterial.material != "":
-        rtMaterial, tScore = process.extractOne(pEquipmentMaterial.material, MATERIAL_FITTINGS)  # 先匹配材质
+    rtMaterial, tScore = _fuzzy_match_material(pEquipmentMaterial)  # 先匹配材质
     rtBestMatch = ""
     rtFlange = 0
     rtScore = 0
     rtType = ""
     tFlangePattern = re.compile(r"(单法|双法|二法|三法|四法)")  # 定义正则表达式匹配法兰数量
-    if rtMaterial in MATERIAL_STEEL_FITTINGS:
+    if rtMaterial in ["球铁"]:
+        tMatchResult = process.extractOne(pEquipmentMaterial.name, Atlas_PipeFittingsDuctileIron.keys())
+        if tMatchResult is not None:  # 检查是否找到匹配
+            tBestMatch, score = tMatchResult
+            if score > rtScore:
+                rtBestMatch = tBestMatch
+                rtScore = score
+                rtType = "管配件"
+    elif rtMaterial in MATERIAL_FITTINGS_STEEL or rtMaterial == "":
         tMatchResult = process.extractOne(pEquipmentMaterial.name, Atlas_PipeFittingsQ235A.keys())
         if tMatchResult is not None:  # 检查是否找到匹配
             tBestMatch, score = tMatchResult
@@ -85,14 +102,6 @@ def fuzzy_match_EM(pEquipmentMaterial):
                     flange_text = tFlangeMatch.group(1)
                     # 将中文数字转换为阿拉伯数字
                     rtFlange = {"单法": 1, "双法": 2, "二法": 2, "三法": 3, "四法": 4}.get(flange_text, 0)
-                rtScore = score
-                rtType = "管配件"
-    elif rtMaterial in ["球铁"]:
-        tMatchResult = process.extractOne(pEquipmentMaterial.name, Atlas_PipeFittingsDuctileIron.keys())
-        if tMatchResult is not None:  # 检查是否找到匹配
-            tBestMatch, score = tMatchResult
-            if score > rtScore:
-                rtBestMatch = tBestMatch
                 rtScore = score
                 rtType = "管配件"
 
